918fff3e40aa9fe28eee59e3b7231a9bfaa143ab
jasinliu <939282975@qq.com>
2024-02-16 14:36:03 +0800

    chore(dep): stylelint init

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+// @see: https://stylelint.io
+/** @type {import('stylelint').Config} */
+export default {
+  root: true,
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-recommended-vue/scss"
+  ]
+};
